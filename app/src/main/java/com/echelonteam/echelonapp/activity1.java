ab94d1e6289d9641381a03ff2b35a2c37c6a3b69
TMDR <tmdrforgit@gmail.com>
2019-01-05 05:59:23 +0200

    button peak needs

--- app/src/main/java/com/echelonteam/echelonapp/activity1.java
+++ app/src/main/java/com/echelonteam/echelonapp/activity1.java
@@ -1,7 +1,11 @@
 package com.echelonteam.echelonapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity1 extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1);
+        Button button=findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri=Uri.parse("https://echelonteam.com");
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(uri);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
