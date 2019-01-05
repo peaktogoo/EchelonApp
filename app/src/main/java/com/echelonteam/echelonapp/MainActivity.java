5691eb8b2fa409e85e32f0853f9706073c239a1a
Krittin Chuaiphikroh <admin@example.com>
2019-01-05 20:40:51 +0700

    Rework of strings

--- app/src/main/java/com/echelonteam/echelonapp/MainActivity.java
+++ app/src/main/java/com/echelonteam/echelonapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Sorry! This feature is not available to you yet", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
