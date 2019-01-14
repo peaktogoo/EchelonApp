cb2c99fe6366ca6e6b672cabfdabbd831fb8bc5b
TMDR <tmdrforgit@gmail.com>
2019-01-14 17:04:39 +0200

    implemented the popup view,still no layout to show,beta and may make it dynamic for multiple buttons

--- app/src/main/java/com/echelonteam/echelonapp/MainActivity.java
+++ app/src/main/java/com/echelonteam/echelonapp/MainActivity.java
@@ -12,11 +12,18 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity
@@ -26,100 +33,123 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Window window=getWindow();
+        Window window = getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(ContextCompat.getColor(this,R.color.black));
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.black));
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button infobutton = findViewById(R.id.infobutton);
+        infobutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String[] emails={"support@echelonteam.com"};
-                Intent intent=new Intent(Intent.ACTION_SEND);
-                intent.setType("*"+"/*");
-                intent.putExtra(Intent.EXTRA_EMAIL,emails);
-                intent.putExtra(Intent.EXTRA_SUBJECT,"Please enter your topic");
-                intent.putExtra(Intent.EXTRA_TEXT,"Please type what you want to say");
-                startActivity(intent);
+                LayoutInflater inflater = (LayoutInflater)
+                        getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.desc_yunus, null);
+                popupView.setAnimation(AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in));
+                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                boolean focusable = true; // lets taps outside the popup also dismiss it
+                final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+                popupView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        popupWindow.dismiss();
+                        return true;
+                    }
+                });
+
+
             }
         });
+                FloatingActionButton fab = findViewById(R.id.fab);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String[] emails = {"support@echelonteam.com"};
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        intent.setType("*" + "/*");
+                        intent.putExtra(Intent.EXTRA_EMAIL, emails);
+                        intent.putExtra(Intent.EXTRA_SUBJECT, "Please enter your topic");
+                        intent.putExtra(Intent.EXTRA_TEXT, "Please type what you want to say");
+                        startActivity(intent);
+                    }
+                });
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                drawer.addDrawerListener(toggle);
+                toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
+                NavigationView navigationView = findViewById(R.id.nav_view);
+                navigationView.setNavigationItemSelectedListener(this);
+            }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            @Override
+            public void onBackPressed() {
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                if (drawer.isDrawerOpen(GravityCompat.START)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    super.onBackPressed();
+                }
+            }
+
+            @Override
+            public boolean onCreateOptionsMenu(Menu menu) {
+                // Inflate the menu; this adds items to the action bar if it is present.
+                getMenuInflater().inflate(R.menu.main, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onOptionsItemSelected(MenuItem item) {
+                // Handle action bar item clicks here. The action bar will
+                // automatically handle clicks on the Home/Up button, so long
+                // as you specify a parent activity in AndroidManifest.xml.
+                int id = item.getItemId();
+
+                //noinspection SimplifiableIfStatement
+                if (id == R.id.action_settings) {
+                    return true;
+                }
+
+                return super.onOptionsItemSelected(item);
+            }
+
+            @SuppressWarnings("StatementWithEmptyBody")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle navigation view item clicks here.
+                int id = item.getItemId();
+
+                ViewFlipper viewFlipper = findViewById(R.id.main_vf);
+
+                if (id == R.id.Info) {
+                    viewFlipper.setDisplayedChild(0);
+                } else if (id == R.id.Members) {
+                    viewFlipper.setDisplayedChild(1);
+                } else if (id == R.id.Projects) {
+                    viewFlipper.setDisplayedChild(2);
+                } else if (id == R.id.Custom_ROMs) {
+                    viewFlipper.setDisplayedChild(3);
+                } else if (id == R.id.Downloads) {
+                    viewFlipper.setDisplayedChild(4);
+                } else if (id == R.id.Wallpapers) {
+                    viewFlipper.setDisplayedChild(5);
+                } else if (id == R.id.Contacts) {
+                    viewFlipper.setDisplayedChild(6);
+                } else if (id == R.id.share) {
+                    Uri uri = Uri.parse("https://t.me/enessastimgroup");
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(uri);
+                    startActivity(intent);
+                }
+                DrawerLayout drawer = findViewById(R.id.drawer_layout);
+                drawer.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        ViewFlipper viewFlipper = findViewById(R.id.main_vf);
-
-        if (id == R.id.Info) {
-            viewFlipper.setDisplayedChild(0);
-        } else if (id == R.id.Members) {
-            viewFlipper.setDisplayedChild(1);
-        } else if (id == R.id.Projects) {
-            viewFlipper.setDisplayedChild(2);
-        } else if (id == R.id.Custom_ROMs) {
-            viewFlipper.setDisplayedChild(3);
-        } else if (id == R.id.Downloads) {
-            viewFlipper.setDisplayedChild(4);
-        } else if (id == R.id.Wallpapers) {
-            viewFlipper.setDisplayedChild(5);
-        } else if (id == R.id.Contacts) {
-            viewFlipper.setDisplayedChild(6);
-        } else if (id == R.id.share) {
-            Uri uri = Uri.parse("https://t.me/enessastimgroup");
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(uri);
-            startActivity(intent);
-        }
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-
-        return true;
-    }
-}
